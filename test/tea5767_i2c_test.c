23a217f41e5b861cc2e0fbfc15d3b98378e5a961
Carlos Egea <charlie.egea@gmail.com>
2025-01-26 11:21:50 +0100

    Extend tests to all functions. Not passing yet. Missing function calls inside tests

--- test/tea5767_i2c_test.c
+++ test/tea5767_i2c_test.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <tea5767_i2c.h>
 #include <unity.h>
 #include <stdio.h>
@@ -6,36 +7,9 @@ void test_TEA5767_Init(void)
 {
     TEA5757_t radio = {0};
     tea5767_init(&radio);
-    
-    // printf("mute_mode %d \n",radio.mute_mode);
-    // TEST_ASSERT(radio.mute_mode == default_cfg.mute_mode);
-    // printf("searchMode %d \n",radio.searchMode);
-    // TEST_ASSERT(radio.searchMode == default_cfg.searchMode);
-    // printf("frequency %f \n",radio.frequency);
-    // // TEST_ASSERT(radio.frequency == default_cfg.frequency);
-    // printf("searchUpDown %d \n",radio.searchUpDown);
-    // TEST_ASSERT(radio.searchUpDown == default_cfg.searchUpDown);
-    // printf("searchLevel %d \n",radio.searchLevel);
-    // TEST_ASSERT(radio.searchLevel == default_cfg.searchLevel);
-    // printf("stereoMode %d \n",radio.stereoMode);
-    // TEST_ASSERT(radio.stereoMode == default_cfg.stereoMode);
-    // printf("muteLmode %d \n",radio.muteLmode);
-    // TEST_ASSERT(radio.muteLmode == default_cfg.muteLmode);
-    // printf("muteRmode %d \n",radio.muteRmode);
-    // TEST_ASSERT(radio.muteRmode == default_cfg.muteRmode);
-    // printf("standby %d \n",radio.standby);
-    // TEST_ASSERT(radio.standby == default_cfg.standby);
-    // printf("band_mode %d \n",radio.band_mode);
-    // TEST_ASSERT(radio.band_mode == default_cfg.band_mode);
-    // printf("softMuteMode %d \n",radio.softMuteMode);
-    // TEST_ASSERT(radio.softMuteMode == default_cfg.softMuteMode);
-    // printf("hpfMode %d \n",radio.hpfMode);
-    // TEST_ASSERT(radio.hpfMode == default_cfg.hpfMode);
-    // printf("stereoNoiseCancelling %d \n",radio.stereoNoiseCancelling);
-    // TEST_ASSERT(radio.stereoNoiseCancelling == default_cfg.stereoNoiseCancelling);
 }
 
-void test_TEA5767_getStation(void)
+void test_TEA5767_getSetStation(void)
 {
     TEA5757_t radio = {0};
     tea5767_init(&radio);
@@ -56,6 +30,7 @@ void test_TEA5767_getReady(void)
     TEA5757_t radio = {0};
     tea5767_init(&radio);
 
+    TEST_ASSERT(radio.read.ready == true);
 }
 
 void test_TEA5767_setSearch(void)
@@ -63,27 +38,27 @@ void test_TEA5767_setSearch(void)
     TEA5757_t radio = {0};
     tea5767_init(&radio);
     
+    TEST_ASSERT(radio.write.searchDownUp == true);
+    TEST_ASSERT(radio.write.searchModeEnabled == true);
 }
 
-void test_TEA5767_setStation(void)
+void test_TEA5767_mute(void)
 {
     TEA5757_t radio = {0};
     tea5767_init(&radio);
     
-}
-
-void test_TEA5767_setMute(void)
-{
-    TEA5757_t radio = {0};
-    tea5767_init(&radio);
-    
+    TEST_ASSERT(radio.write.mute == true);
+    TEST_ASSERT(radio.write.softMute == true);
+    TEST_ASSERT(radio.write.muteL == true);
+    TEST_ASSERT(radio.write.muteR == true);
 }
 
 void test_TEA5767_setStadby(void)
 {
     TEA5757_t radio = {0};
     tea5767_init(&radio);
-    
+
+    TEST_ASSERT(radio.write.standy == true);    
 }
 
 void test_TEA5767_setStereo(void)
@@ -91,6 +66,7 @@ void test_TEA5767_setStereo(void)
     TEA5757_t radio = {0};
     tea5767_init(&radio);
     
+    TEST_ASSERT(radio.read.stereo == true);    
 }
 
 void setUp(void)
@@ -105,7 +81,11 @@ int main(void)
 {
     UNITY_BEGIN();
     RUN_TEST(test_TEA5767_Init);
-    RUN_TEST(test_TEA5767_getStation);
+    RUN_TEST(test_TEA5767_getSetStation);
+    RUN_TEST(test_TEA5767_getReady);
+    RUN_TEST(test_TEA5767_setSearch);
+    RUN_TEST(test_TEA5767_mute);
+    RUN_TEST(test_TEA5767_setStadby);
 
     return UNITY_END();
 }
